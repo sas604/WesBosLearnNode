c8c0ce1c3c2e228b2281a2371f17a6be05e8e2fd
Aleksandr Tagunov <aleksandr@tagunovdesign.com>
2021-02-06 13:44:12 -0800

    query stores from the database and display them

--- dang-del/controllers/storeController.js
+++ dang-del/controllers/storeController.js
@@ -1,8 +1,30 @@
+const mongoose = require('mongoose');
+
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
-  console.log(req.name);
   res.render('index');
 };
 
 exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add store' });
+};
+
+exports.createStore = (req, res) => {
+  res.json(req.body);
+};
+exports.createStore = async (req, res) => {
+  const store = await new Store(req.body).save();
+  req.flash(
+    'success',
+    `Succesfully Created ${store.name}. Care to leve a review?`
+  );
+  res.redirect(`/store/${store.slug}`);
+};
+
+exports.getStores = async (req, res) => {
+  // TODO Query database for a list of all stores
+  const stores = await Store.find();
+
+  res.render('stores', { title: 'Stores', stores });
 };
