d7831979c82f847506b06d9522a2cc3e408e6066
Aleksandr Tagunov <aleksandr@tagunovdesign.com>
2021-02-23 11:41:21 -0800

    front-end search results

--- dang-del/public/javascripts/modules/typeAhead.js
+++ dang-del/public/javascripts/modules/typeAhead.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import dompurify from 'dompurify';
 
 function searchResultsHTML(stores) {
   return stores
@@ -25,20 +26,48 @@ function typeAhead(search) {
     }
     // show the search result
     searchResults.style.display = 'block';
-    searchResults.innerHTML = '';
     axios
       .get(`/api/search?q=${this.value}`)
       .then((res) => {
         if (res.data.length) {
           const html = searchResultsHTML(res.data);
-          searchResults.innerHTML = html;
+          searchResults.innerHTML = dompurify.sanitize(html);
+          return;
         }
+        // nothing is found
+        searchResults.innerHTML = dompurify.sanitize(
+          `<div class="search__result"> No results for ${this.value} found!</div>`
+        );
       })
       .catch((err) => console.error(err));
   });
 
   // handle keyboard inputs
-  searchInput.on('keyup', (e) => console.log(e.keyCode));
+  searchInput.on('keyup', (e) => {
+    // if they aren't pressing up, down or enter, skip it
+    if (![38, 40, 13].includes(e.keyCode)) return;
+    const activeClass = 'search__result--active';
+    const current = search.querySelector(`.${activeClass}`);
+    const items = search.querySelectorAll('.search__result');
+    let next;
+    if (e.keyCode === 40 && current) {
+      console.log('ss');
+      next = current.nextElementSibling || items[0];
+    } else if (e.keyCode === 40) {
+      [next] = items;
+    } else if (e.keyCode === 38 && current) {
+      next = current.previousElementSibling || items[items.length - 1];
+    } else if (e.keyCode === 38) {
+      next = items[items.length - 1];
+    } else if (e.keyCode === 13 && current.href) {
+      window.location = current.href;
+      return;
+    }
+    if (current) {
+      current.classList.remove(activeClass);
+    }
+    next.classList.add(activeClass);
+  });
 }
 
 export default typeAhead;
